2117ca0d4b9f38aa3f691046abfda18794a2c1fc
Andreas Krahl <andreas.krahl@netzkern.de>
2019-10-20 00:41:23 +0200

    fitst commit

---
+++ GameHub/GameHub.cs
@@ -0,0 +1,40 @@
+﻿namespace GameHub
+{
+    using System.Threading.Tasks;
+    using AppHub;
+
+    public class GameHub : AppHub<IGameClient>
+    {
+        public IGameClient GameClient { get; private set; }
+
+
+        public GameHub()
+        {
+           
+        }
+
+        //public override Task AddCurrentUser(string userName, bool isAnonymous = true)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+
+        //public override Task<string> JoinGroup(string groupName)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+
+        //public override Task<string> LeaveGroup(string groupName)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+    }
+    public interface IGameClient : IAppClient
+    {
+        //Task Hello(string message);
+        //Task SwitchTurn();
+
+        //Task TileChange(string tileId);
+
+        //Task GameOver(string winningTileId, string winningLine);
+    }
+}
